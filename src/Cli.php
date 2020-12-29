131870e3b44d88de2816fc9ed1f252c8b7e1be51
Eugeniy Galkovskiy <zhegal@gmail.com>
2020-12-29 18:03:33 +0200

    step three

---
+++ src/Cli.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Brain\Games;
+
+class Cli
+{
+    private string $name;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+}
